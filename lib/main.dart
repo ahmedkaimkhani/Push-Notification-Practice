be8b8a62c165edec260e9f41eb2612e9b63f354b
ahmedkaimkhani <ak9302359@gmail.com>
2023-11-18 02:57:50 +0500

    Push Notification

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeView(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeView(),
     );
   }
 }
