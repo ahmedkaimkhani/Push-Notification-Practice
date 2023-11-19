8c6fb7cc65ba557d83da0f32c555251ef91d70b0
ahmedkaimkhani <ak9302359@gmail.com>
2023-11-19 22:18:30 +0500

    Push Notification

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:push_notification_practice/notification_view.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  NotificationService notificationService = NotificationService();
+
+  @override
+  void initState() {
+    super.initState();
+    notificationService.requestNotificationPermission();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:push_notification_practice/firebase_options.dart';
 import 'package:push_notification_practice/home_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await DefaultFirebaseOptions.currentPlatform;
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
